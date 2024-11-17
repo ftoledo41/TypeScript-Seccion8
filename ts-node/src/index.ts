c8f775c9f90ef8c4abe8c7792acdbc229a923be9
ftoledo43 <franciscojaviertoledocollante@gmail.com>
2024-11-17 20:00:18 -0300

    first commit

---
+++ ts-node/src/index.ts
@@ -0,0 +1,12 @@
+import { Hero } from './classes/Hero'
+// import * as HeroeClases from "./classes/Hero";
+// import powers2, {Power} from "./data/powres";
+
+const ironman = new Hero("Francisco", 3, 4);
+
+console.log(ironman.power)
+
+
+
+
+
